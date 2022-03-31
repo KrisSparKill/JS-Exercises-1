5f880487e36ca530bf3e707b48c9fe45633c75ba
Glitch (45-js-exercises) <none>
2022-03-31 21:43:10 +0000

    🎳👧 Checkpoint ./index.html:282805/1142 ./style.css:282805/51 ./client.js:282805/104

--- client.js
+++ client.js
@@ -2,8 +2,10 @@
 
 // projectName and projectEditLinks
 const projectName = location.host.substring(0, location.host.indexOf("."));
+const projectUrl = "https://" + projectName + ".glitch.me"
 const editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=45-exercises.js";
 
-const links = Array.from(document.querySelectorAll(".solutions-edit-link"));
 
+document.querySelectorAll(projectUrl)
+const links = Array.from(document.querySelectorAll(".solutions-edit-link"));
 links.map(link => link.href = editSolutionsUrl);
