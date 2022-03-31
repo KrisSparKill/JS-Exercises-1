a1eb197bf6a392dba564bc2f6c3d44600bfc9b4b
Glitch (45-js-exercises) <none>
2022-03-31 20:10:00 +0000

    🕶😋 Checkpoint ./client.js:282805/210 ./index.html:282805/215

--- client.js
+++ client.js
@@ -3,4 +3,7 @@
 // projectName and projectEditLink
 const projectName = location.host.substring(0, location.host.indexOf("."));
 const editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js";
-document.getElementById("solutions-edit-link").href = editSolutionsUrl;
+
+const links = Array.from(document.querySelectorAll(".solutions-edit-link"));
+
+links.map(link => link.href = editSolutionsUrl);
